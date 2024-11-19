a6b16b8bd3dcd11ae0684576445ed2b08cc77ae7
DivineDemon <supame123@gmail.com>
2024-11-19 16:21:11 +0500

    Clerk Bug Fixes!

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -32,7 +32,7 @@ const Page = async ({ searchParams }: PageProps) => {
   });
 
   if (!user) {
-    redirect("/sign-in");
+    redirect("/welcome");
   }
 
   const { intent, success } = await searchParams;
